0b266f4d1f11599200144243e67507c8fcaee464
Isaque Roberto <71456433+IsaqueR02@users.noreply.github.com>
2025-09-04 19:26:55 +0000

    feat: verificação do nome da tarefa

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-let lista = document.getElementById("task");
-let tarefa = document.getElementById("new-task")
+let lista = document.getElementById("list-task");
+let tarefa = document.getElementById("new-task");
+let btnTask = document.getElementById("add-task");
+adicionarTarefa()
 
 function adicionarTarefa() {
+    tarefa = document.getElementById("new-task").value;
+    /**
+     * Verifica se o input não está vazio
+     */
+    if(tarefa.value == '') {
+        alert('Digite o nome da tarefa!');
+        return;
+    }
+
+    if (tarefas.includes(tarefa.value)) {
+        alert('Tarefa adicionada!')
+        return;
+    }
     
+    let itemList = document.createElement("li")
+    itemList.push(tarefa.value);
+
+    if (lista.textContent == '') {
+        lista.textContent = tarefa.value;
+    } else {
+    lista.textContent = lista.textContent + ', ' + tarefa.value;
+    }
+    tarefa.value = '';
 }
